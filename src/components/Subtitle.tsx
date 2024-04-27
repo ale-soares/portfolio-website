b254c1e0cf1ce6113331e960f9c9fd5b8d9c86a7
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-04-27 13:24:14 -0300

    add subtitle component

---
+++ src/components/Subtitle.tsx
@@ -0,0 +1,26 @@
+interface SubtitleProps {
+  label: string;
+  isSectionTitle: boolean;
+}
+
+const Subtitle: React.FC<SubtitleProps> = ({ label, isSectionTitle }) => {
+  return (
+    <>
+      {isSectionTitle ?
+        (
+          <div className="relative flex py-5 items-center">
+            <span className="flex-shrink mr-4 font-sans text-section text-theme-white">{label}</span>
+            <div className="flex-grow border-t border-theme-white" />
+          </div>
+        ) :
+        (
+          <h3 className="font-sans text-section text-theme-white">
+            {label}
+          </h3>
+        )
+      }
+    </>
+  );
+};
+
+export default Subtitle;
--- src/sections/ComponentPreview.tsx
+++ src/sections/ComponentPreview.tsx
@@ -2,6 +2,7 @@ import Button from "./../components/Button";
 import Link from "./../components/Link";
 import Tag from "../components/Tag";
 import Card from "../components/Card";
+import Subtitle from "../components/Subtitle";
 
 import github from "../assets/icons/github.png"
 
@@ -17,6 +18,7 @@ const ComponentPreview: React.FC = () => {
       <Tag label="React.js"/>
       <Card description="dsadsa" titleUrl="http://localhost:5173/" titleLabel="dasdas"/>
       <Link isIcon={true} linkUrl="http://localhost:5173/" iconSrc={ github } iconAlt="das"/>
+      <Subtitle isSectionTitle={true} label="dsadsa"/>
     </>
   );
 }

bf676941c932055e4ae9e2ef3c45433853023e4c
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-05-02 16:49:39 -0300

    make sidebar sticky

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -4,16 +4,18 @@ import { data } from "../mocks/Navbar"
 
 const Navbar = () => {
   return (
-    <div className="flex justify-end">
-      <div className="flex w-40 justify-between">
-        {data.map(e => (
-          <Link 
-            isIcon={true}
-            linkUrl={e.url}
-            iconSrc={e.src}
-            iconAlt={e.name}
-          />
-        ))}
+    <div className="sticky top-6">
+      <div className="flex justify-end">
+        <div className="flex w-40 justify-between">
+          {data.map(e => (
+            <Link 
+              isIcon={true}
+              linkUrl={e.url}
+              iconSrc={e.src}
+              iconAlt={e.name}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );

54f83234c50913956eb9944ea758f55f399024b9
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-05-02 16:38:21 -0300

    add particles bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-// import ComponentPreview from "./sections/ComponentPreview";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
 import Other from "./sections/Other";
+import Effects from "./sections/Effects";
 
 const App: React.FC = () => {
   return (
-    <div className="p-6">
-    <Navbar />
-      <div className="px-80 pb-40">
-        <Hero />
-        <Skills />
-        <Projects />
-        <Other />
+    <>
+      <Effects />
+      <div className="p-6">
+      <Navbar />
+        <div className="px-80 pb-40">
+          <Hero />
+          <Skills />
+          <Projects />
+          <Other />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/sections/Effects.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+
+const Effects = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 200,
+          enable: true,
+          opacity: 0.1,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          random: false,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.1,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 2 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default Effects;

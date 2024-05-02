99b9c88f0e55e6ee3c76f1e4c22fa24ff819e5b4
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-05-02 17:11:56 -0300

    open links in new tab

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -8,7 +8,12 @@ interface LinkProps {
 
 const Link: React.FC<LinkProps> = ({ label, linkUrl, isIcon, iconSrc, iconAlt }) => {
   return (
-    <a href={linkUrl} className="underline font-mono text-body text-theme-blue">
+    <a 
+      href={linkUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="underline font-mono text-body text-theme-blue"
+    >
       {isIcon ? (
         <img className="w-8" src={iconSrc} alt={iconAlt} />
       ) : label}
